cb447587b9dd358b4b09e634c9e0e25354008b29
Pernille Klevstuen <peklevstuen@gmail.com>
2019-09-11 20:51:50 +0200

    :clown_face: Created mockapi

--- src/app.ts
+++ src/app.ts
@@ -5,21 +5,18 @@ import bodyParser from "body-parser";
 const app = express();
 
 //Controllers (route handlers)
-
+import * as testController from "./controllers/TestController";
 
 app.set("port", process.env.PORT || 8080);
 // Bodyparser middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ 
-  extended: true 
-}));
-
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 //Primary app routes
-/*TODO
-ex:
-app.get("/", homeController.index);
-*/
-
+app.get("/", testController.index);
 
 export default app;
--- src/controllers/TestController.ts
+++ src/controllers/TestController.ts
@@ -1,3 +1,5 @@
-export class TestController {
+import { Request, Response } from "express";
 
-}
+export const index = (req: Request, res: Response) => {
+  return res.status(200).json({ pernille: "hei" });
+};
