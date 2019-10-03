868ef35b9f12a384dac0d9753f64621dcf754898
Sindre Sivertsen <sindrejohan1@gmail.com>
2019-10-03 21:31:18 +0200

    :100: started adding create tables in init-db.sql script

--- postgres/sql/init.sql
+++ postgres/sql/init.sql
@@ -4,5 +4,48 @@ CREATE DATABASE photodb;
 
 CREATE TABLE IF NOT EXISTS photo (
     id serial PRIMARY KEY,
-    motive text NOT NULL
+    motive text NOT NULL,
+	motive_id INTEGER REFERENCES motive(id)E  PRIMARY KEY,
+	place_id INTEGER REFERENCES place(id)
+);
+
+CREATE TABLE IF NOT EXISTS analog (
+	-- TODO
+) INHERITS (photo);
+
+CREATE TABLE IF NOT EXISTS album (
+    id serial PRIMARY KEY,
+);
+CREATE TABLE IF NOT EXISTS place (
+    id serial PRIMARY KEY,
+);
+CREATE TABLE IF NOT EXISTS motive (
+    id serial PRIMARY KEY,
+);
+CREATE TABLE IF NOT EXISTS category (
+    id serial PRIMARY KEY,
+);
+CREATE TABLE IF NOT EXISTS tag (
+    id serial PRIMARY KEY,
+);
+CREATE TABLE IF NOT EXISTS tag-photo (
+    id serial PRIMARY KEY,
+);
+CREATE TABLE IF NOT EXISTS tag (
+    id serial PRIMARY KEY,
+);
+CREATE TABLE IF NOT EXISTS photographer (
+    id serial PRIMARY KEY,
+);
+CREATE TABLE IF NOT EXISTS security(
+    id serial PRIMARY KEY,
+);
+CREATE TABLE IF NOT EXISTS order(
+    id serial PRIMARY KEY,
+);
+CREATE TABLE IF NOT EXISTS orderphotos(
+    id serial PRIMARY KEY,
+);
+CREATE TABLE IF NOT EXISTS orderphotos(
+    id serial PRIMARY KEY,
 );
