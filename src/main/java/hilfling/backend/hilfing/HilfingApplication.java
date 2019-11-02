684564d44b596b9aed9a75c57809157e9f502298
Sindre Sivertsen <sindrejohan1@gmail.com>
2019-11-02 16:55:38 +0100

    added some testdata

--- src/main/java/hilfling/backend/hilfing/HilfingApplication.java
+++ src/main/java/hilfling/backend/hilfing/HilfingApplication.java
@@ -1,7 +1,13 @@
 package hilfling.backend.hilfing;
 
+import hilfling.backend.hilfing.model.*;
+import hilfling.backend.hilfing.repositories.CategoryRepository;
+import hilfling.backend.hilfing.repositories.PhotoRepository;
+import hilfling.backend.hilfing.service.CategoryService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HilfingApplication {
@@ -9,5 +15,17 @@ public class HilfingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HilfingApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner demoData(CategoryService categoryService) {
+		//Motive firstMotive = new Motive();
+
+
+		return args -> {
+			categoryService.createCategory(new Category("Fotostand"));
+			System.out.println(categoryService.getCategories());
+		};
+	}
+
 
 }
