256bc9b2c5340b28bfc52e977940209eda326562
Caroline Sandsbråten <carolinesandsbraten@gmail.com>
2019-11-03 17:14:05 +0100

    :bug: fix to work with new generic classes

--- src/main/java/hilfling/backend/hilfing/controllers/PlaceController.java
+++ src/main/java/hilfling/backend/hilfing/controllers/PlaceController.java
@@ -3,41 +3,14 @@ package hilfling.backend.hilfing.controllers;
 import hilfling.backend.hilfing.model.Place;
 import hilfling.backend.hilfing.service.PlaceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/places")
-public class PlaceController {
+public class PlaceController extends GenericBaseControllerImplementation<Place> {
     @Autowired
-    private PlaceService placeService;
-
-    @PostMapping
-    public ResponseEntity<Place> createPlace(@Valid @RequestBody Place place) {
-        return placeService.createPlace(place);
+    private PlaceService service;
+    public PlaceService getService() {
+        return service;
     }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Place> getPlace(@PathVariable("id") Long id) {
-        return placeService.getPlace(id);
-    }
-
-    @GetMapping
-    public ResponseEntity<List<Place>> getAllPlaces() {
-        return placeService.getAllPlaces();
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Place> updatePlace(@Valid @RequestBody Place place, @PathVariable("id") Long id) {
-        return placeService.updatePlace(place, id);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> deletePlace(@PathVariable("id") Long id) {
-        return placeService.deletePlace(id);
-    }
-
 }
--- src/main/java/hilfling/backend/hilfing/service/PlaceService.java
+++ src/main/java/hilfling/backend/hilfing/service/PlaceService.java
@@ -3,49 +3,14 @@ package hilfling.backend.hilfing.service;
 import hilfling.backend.hilfing.model.Place;
 import hilfling.backend.hilfing.repositories.PlaceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
-public class PlaceService {
+public class PlaceService extends
+        GenericBaseServiceImplementation<Place> {
     @Autowired
-    PlaceRepository placeRepository;
-
-    public ResponseEntity<Place> createPlace(Place place) {
-        try {
-            Place savedPlace = placeRepository.save(place);
-            return ResponseEntity.ok().body(savedPlace);
-        } catch (Exception e) {
-            return ResponseEntity.status(304).build();
-        }
-    }
-
-    public ResponseEntity<Place> getPlace(Long id) {
-        return placeRepository.findById(id)
-                .map(place -> ResponseEntity.ok().body(place))
-                .orElse(ResponseEntity.notFound().build());
-    }
-
-    public ResponseEntity<List<Place>> getAllPlaces() {
-        List<Place> places = placeRepository.findAll();
-        return ResponseEntity.ok().body(places);
-    }
-
-    public ResponseEntity<Place> updatePlace(Place place, Long id) {
-        if (placeRepository.findById(id).isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-        place.setId(id);
-        return ResponseEntity.ok().body(placeRepository.save(place));
-    }
-
-    public ResponseEntity<?> deletePlace(Long id) {
-        return placeRepository.findById(id)
-                .map(place -> {
-                    placeRepository.deleteById(id);
-                    return ResponseEntity.ok().build();
-                }).orElse(ResponseEntity.notFound().build());
+    PlaceRepository repository;
+    public PlaceRepository getRepository() {
+        return repository;
     }
 }
