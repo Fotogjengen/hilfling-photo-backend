35c0a0d611844716754bc3e9d171b4b0b76d77ef
Caroline Sandsbråten <carolinesandsbraten@gmail.com>
2020-01-28 22:50:06 +0100

    :ok_hand: fix small things reqursted by pernilak

--- src/main/java/hilfling/backend/hilfing/controllers/ArticleTagController.java
+++ src/main/java/hilfling/backend/hilfing/controllers/ArticleTagController.java
@@ -3,8 +3,6 @@ package hilfling.backend.hilfing.controllers;
 import hilfling.backend.hilfing.model.ArticleTag;
 import hilfling.backend.hilfing.service.ArticleTagService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,10 +12,5 @@ public class ArticleTagController extends GenericBaseControllerImplementation<Ar
     private ArticleTagService service;
     public ArticleTagService getService() {
         return service;
-    }
-
-    @Override
-    public ResponseEntity<ArticleTag> getAll() {
-        return super.getAll();
     }
 }
