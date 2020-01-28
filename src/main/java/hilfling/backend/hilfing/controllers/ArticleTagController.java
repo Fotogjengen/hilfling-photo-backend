62d95d68d26d91fc971ac3126f7be10c7d521759
Caroline Sandsbråten <carolinesandsbraten@gmail.com>
2020-01-28 22:35:24 +0100

    :sparkles: add auth

--- src/main/java/hilfling/backend/hilfing/controllers/ArticleTagController.java
+++ src/main/java/hilfling/backend/hilfing/controllers/ArticleTagController.java
@@ -3,6 +3,8 @@ package hilfling.backend.hilfing.controllers;
 import hilfling.backend.hilfing.model.ArticleTag;
 import hilfling.backend.hilfing.service.ArticleTagService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,5 +14,10 @@ public class ArticleTagController extends GenericBaseControllerImplementation<Ar
     private ArticleTagService service;
     public ArticleTagService getService() {
         return service;
+    }
+
+    @Override
+    public ResponseEntity<ArticleTag> getAll() {
+        return super.getAll();
     }
 }
