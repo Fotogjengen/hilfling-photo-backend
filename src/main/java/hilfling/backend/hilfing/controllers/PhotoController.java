c08d6bc6f15da642b8bd6474ecc61d23a1bb7e16
Sindre Sivertsen <sindrejohan1@gmail.com>
2020-02-08 16:54:33 +0100

    :ok_hand: endret endpoints så alle ender med s

--- src/main/java/hilfling/backend/hilfing/controllers/PhotoController.java
+++ src/main/java/hilfling/backend/hilfing/controllers/PhotoController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/photo")
+@RequestMapping("/api/v1/photos")
 public class PhotoController extends GenericBaseControllerImplementation<Photo> {
     @Autowired
     private PhotoService service;
