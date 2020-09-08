cb15d52f3fa62cd18cafe2af4b34e16c735846c3
Caroline Sandsbråten <carolinesandsbraten@gmail.com>
2020-09-08 19:30:01 +0200

    fix auth (turn off tmp)

--- src/main/java/hilfling/backend/hilfling/configurations/ResourceServerConfiguration.java
+++ src/main/java/hilfling/backend/hilfling/configurations/ResourceServerConfiguration.java
@@ -21,8 +21,9 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().authenticated();
+        /*http.authorizeRequests()
+                .anyRequest().authenticated();*/
+        http.authorizeRequests().antMatchers("/").permitAll();
     }
 
     @Override
