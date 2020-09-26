dd9ffe89492893f9da05ede92d0524969eda6ba4
Caroline Sandsbråten <carolinesandsbraten@gmail.com>
2020-09-26 15:05:47 +0200

    change names

--- src/main/java/hilfling/backend/hilfing/model/Base.kt
+++ src/main/java/hilfling/backend/hilfing/model/Base.kt
@@ -6,13 +6,13 @@ import me.liuwj.ktorm.schema.date
 import me.liuwj.ktorm.schema.long
 import java.time.LocalDate
 
-open interface BaseModel<T: Entity<T>>: Entity<T> {
+open interface BaseModel<E: Entity<E>>: Entity<E> {
     //companion object : Entity.Factory<T>()
     val id: Long
     var dateCreated: LocalDate
 }
 
-open class BaseTable<T: BaseModel<T>>(tableName: String) : Table<T>(tableName){
+open class BaseTable<E: BaseModel<E>>(tableName: String) : Table<E>(tableName){
     val id = long("id").primaryKey().bindTo { it.id }
     val dateCreated = date("date_created").bindTo{it.dateCreated}
 
