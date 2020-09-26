377b9f28abb457d08a550fc3fded6462f03cc8b0
Caroline Sandsbråten <carolinesandsbraten@gmail.com>
2020-09-26 23:59:06 +0200

    create purchase order controller

---
+++ src/main/kotlin/hilfling/backend/hilfling/controller/PurchaseOrderController.kt
@@ -0,0 +1,32 @@
+package hilfling.backend.hilfling.controller
+
+import hilfling.backend.hilfling.model.Album
+import hilfling.backend.hilfling.model.PurchaseOrder
+import hilfling.backend.hilfling.repository.AlbumRepository
+import hilfling.backend.hilfling.repository.PurchaseOrderRepository
+import me.liuwj.ktorm.database.Database
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/purchase_orders")
+class PurchaseOrderController {
+    val repository = PurchaseOrderRepository()
+
+    @GetMapping("/{id}")
+    fun getById(@PathVariable("id") id: Long) : PurchaseOrder? {
+        return repository.findById(id)
+    }
+
+    @GetMapping
+    fun getAll() : List<PurchaseOrder> {
+        return repository.findAll()
+    }
+
+    @PostMapping
+    fun create(
+            @RequestParam("purchaseOrder") purchaseOrder: PurchaseOrder
+    ): PurchaseOrder {
+        return repository.create(purchaseOrder)
+    }
+}
