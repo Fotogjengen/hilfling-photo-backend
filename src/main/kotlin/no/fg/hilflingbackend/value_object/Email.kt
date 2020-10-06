eebf5838beee8e08c0e75217f420f984d562437d
Sindre Sivertsen <sindrejohan1@gmail.com>
2020-10-06 20:18:33 +0200

    Fix email value object. Add EmailValidator test

--- src/main/kotlin/no/fg/hilflingbackend/value_object/Email.kt
+++ src/main/kotlin/no/fg/hilflingbackend/value_object/Email.kt
@@ -1,7 +1,26 @@
 package no.fg.hilflingbackend.value_object
 
+import java.lang.IllegalArgumentException
+import java.util.regex.Pattern
+
 data class Email private constructor(val value: String) {
-    init {
-        require(value.isNotBlank())
+    companion object {
+        fun create(value: String): Email {
+            if(value != null && isEmailValid(value)){
+                return Email(value = value)
+            }
+            throw IllegalArgumentException("Not an email")
+        }
+        fun isEmailValid(email: String): Boolean {
+          return Pattern.compile(
+            "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]|[\\w-]{2,}))@"
+            + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+            + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+            + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+            + "[0-9]{1,2}|25[0-5]|2[0-4][0-9]))|"
+            + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$"
+          ).matcher(email).matches()
+        }
+
     }
 }
