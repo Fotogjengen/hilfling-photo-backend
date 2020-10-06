82e7a4935a7f6adc0129772aaed4c1ea23a62ee4
Sindre Sivertsen <sindrejohan1@gmail.com>
2020-10-06 20:18:33 +0200

    Fix email value object. Add EmailValidator test

--- src/main/kotlin/no/fg/hilflingbackend/model/User.kt
+++ src/main/kotlin/no/fg/hilflingbackend/model/User.kt
@@ -6,6 +6,7 @@ import me.liuwj.ktorm.entity.sequenceOf
 import me.liuwj.ktorm.schema.*
 import no.fg.hilflingbackend.model.BaseModel
 import no.fg.hilflingbackend.model.BaseTable
+import no.fg.hilflingbackend.value_object.Email
 
 interface User : BaseModel<User> {
     companion object : Entity.Factory<User>()
@@ -13,7 +14,7 @@ interface User : BaseModel<User> {
     var firstName: String
     var lastName: String
     var username: String
-    var email: String
+    var email: Email
     var profilePicture: String
     var phoneNumber: String
     var sex: String
@@ -23,7 +24,7 @@ object Users : BaseTable<User>("user") {
     val firstName = varchar("first_name").bindTo { it.firstName }
     val lastName = varchar("last_name").bindTo { it.lastName }
     val username = varchar("username").bindTo { it.username }
-    val email = varchar("email").bindTo { it.email }
+    val email = varchar("email").bindTo { it.email.value  }
     val profilePicture = varchar("profile_picture").bindTo { it.profilePicture }
     val phoneNumber = varchar("phone_number").bindTo { it.phoneNumber }
     val sex = varchar("sex").bindTo { it.sex }
