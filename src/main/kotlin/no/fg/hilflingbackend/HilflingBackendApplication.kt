8eea325412fec4b90da23afa3ffb7dd5f9607a57
Caroline Sandsbråten <carolinesandsbraten@gmail.com>
2020-10-17 12:41:25 +0200

    rename photo file paths init function

--- src/main/kotlin/no/fg/hilflingbackend/HilflingBackendApplication.kt
+++ src/main/kotlin/no/fg/hilflingbackend/HilflingBackendApplication.kt
@@ -1,11 +1,9 @@
 package no.fg.hilflingbackend
 
-import no.fg.hilflingbackend.repository.AlbumRepository
 import no.fg.hilflingbackend.service.PhotoService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 
@@ -16,7 +14,7 @@ open class HilflingBackendApplication {
 
 	@Bean
 	open fun run() = CommandLineRunner {
-		photoService.init()
+		photoService.createFilesystemIfNotExists() // TODO: Don't run in prod
 	}
 }
 
