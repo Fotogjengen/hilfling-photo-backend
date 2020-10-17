f286d39a2066d57aec2e105996e2287c606d6898
Caroline Sandsbråten <carolinesandsbraten@gmail.com>
2020-10-17 16:09:01 +0200

    use uuid as id

--- src/main/kotlin/no/fg/hilflingbackend/model/Base.kt
+++ src/main/kotlin/no/fg/hilflingbackend/model/Base.kt
@@ -4,15 +4,17 @@ import me.liuwj.ktorm.entity.Entity
 import me.liuwj.ktorm.schema.Table
 import me.liuwj.ktorm.schema.date
 import me.liuwj.ktorm.schema.int
+import me.liuwj.ktorm.schema.uuid
 import java.time.LocalDate
+import java.util.*
 
 open interface BaseModel<E: Entity<E>>: Entity<E> {
-    val id: Int
+    val id: UUID
     var dateCreated: LocalDate
 }
 
 open class BaseTable<E: BaseModel<E>>(tableName: String) : Table<E>(tableName){
-    val id = int("id").primaryKey().bindTo { it.id }
+    val id = uuid("id").primaryKey().bindTo { it.id }
     val dateCreated = date("date_created").bindTo{it.dateCreated}
 
 }
