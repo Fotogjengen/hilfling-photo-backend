4f6aa3ae097420a3725a792d49618badce1a8bd8
Caroline Sandsbråten <carolinesandsbraten@gmail.com>
2020-11-03 20:48:17 +0100

    category now using generics

--- src/main/kotlin/no/fg/hilflingbackend/controller/CategoryController.kt
+++ src/main/kotlin/no/fg/hilflingbackend/controller/CategoryController.kt
@@ -1,5 +1,6 @@
 package no.fg.hilflingbackend.controller
 
+import no.fg.hilflingbackend.dto.CategoryDto
 import no.fg.hilflingbackend.model.Category
 import no.fg.hilflingbackend.repository.CategoryRepository
 import org.springframework.beans.factory.annotation.Qualifier
@@ -7,5 +8,5 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/categories")
-open class CategoryController(override val repository: CategoryRepository) : BaseController<Category, Category>(repository) {
+open class CategoryController(override val repository: CategoryRepository) : BaseController<Category, CategoryDto>(repository) {
 }
--- src/main/kotlin/no/fg/hilflingbackend/repository/CategoryRepository.kt
+++ src/main/kotlin/no/fg/hilflingbackend/repository/CategoryRepository.kt
@@ -3,24 +3,24 @@ package no.fg.hilflingbackend.repository
 import me.liuwj.ktorm.database.Database
 import me.liuwj.ktorm.dsl.*
 import me.liuwj.ktorm.entity.*
+import no.fg.hilflingbackend.dto.CategoryDto
+import no.fg.hilflingbackend.dto.CategoryId
+import no.fg.hilflingbackend.dto.toEntity
 import no.fg.hilflingbackend.model.*
 import org.springframework.stereotype.Repository
 
 @Repository
-open class CategoryRepository(database: Database) : BaseRepository<Category, Category>(table = Categories, database) {
-  override fun convertToClass(qrs: QueryRowSet): Category {
-    return Category{
-      id = qrs[Categories.id]!!
-      dateCreated = qrs[Categories.dateCreated]!!
-      name = qrs[Categories.name]!!
-    }
+open class CategoryRepository(database: Database) : BaseRepository<Category, CategoryDto>(table = Categories, database) {
+  override fun convertToClass(qrs: QueryRowSet): CategoryDto = CategoryDto(
+    categoryId = CategoryId(qrs[Categories.id]!!),
+    name = qrs[Categories.name]!!
+  )
+
+  override fun create(dto: CategoryDto): Int {
+    return database.categories.add(dto.toEntity())
   }
 
-  override fun create(entity: Category): Int {
-    return database.categories.add(entity)
-  }
-
-  override fun patch(entity: Category): Int {
-    return database.categories.update(entity)
+  override fun patch(dto: CategoryDto): Int {
+    return database.categories.update(dto.toEntity())
   }
 }
