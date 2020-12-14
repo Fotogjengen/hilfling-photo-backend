6e4014992ef8fd36416dd1760ca951e1fc2abde5
Sindre J. I. Sivertsen <sindrejohan1@gmail.com>
2020-12-14 20:28:20 +0100

    Add healthCheackController (#54)
    * Add healthCheackController
    
    * Ktlint fixes
    
    * Add HealthControllerSpek
    
    * fix Ktlint errors
    
    * Update ci-cd.yml
    
    * Update ci-cd.yml
    
    * Update ci-cd.yml
    
    * Split CI-CD into PRs and deplyment
    
    * Fix test

---
+++ src/main/kotlin/no/fg/hilflingbackend/controller/HealthController.kt
@@ -0,0 +1,15 @@
+package no.fg.hilflingbackend.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/health")
+class HealthController {
+
+  @GetMapping
+  fun getHealth(): String {
+    return "OK!"
+  }
+}
