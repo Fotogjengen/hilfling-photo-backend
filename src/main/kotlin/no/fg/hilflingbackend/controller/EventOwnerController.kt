c8c59ee48da080d382d1e8cad7af0c9f9e225cdb
Caroline Sandsbråten <carolinesandsbraten@gmail.com>
2021-02-09 20:48:33 +0100

    update eventowner controller and repository

--- src/main/kotlin/no/fg/hilflingbackend/controller/EventOwnerController.kt
+++ src/main/kotlin/no/fg/hilflingbackend/controller/EventOwnerController.kt
@@ -1,5 +1,6 @@
 package no.fg.hilflingbackend.controller
 
+import no.fg.hilflingbackend.dto.EventOwnerDto
 import no.fg.hilflingbackend.model.EventOwner
 import no.fg.hilflingbackend.repository.EventOwnerRepository
 import org.springframework.beans.factory.annotation.Autowired
@@ -13,26 +14,4 @@ import java.util.UUID
 
 @RestController
 @RequestMapping("/event_owners")
-class EventOwnerController {
-  @Autowired
-  lateinit var repository: EventOwnerRepository
-
-  @GetMapping("/{id}")
-  fun getById(@PathVariable("id") id: UUID): EventOwner? {
-    return repository.findById(id)
-  }
-
-  @GetMapping
-  fun getAll(): List<EventOwner> {
-    return repository.findAll()
-  }
-
-  @PostMapping
-  fun create(
-    @RequestBody eventOwner: EventOwner
-  ): Int {
-    return repository.create(
-      eventOwner
-    )
-  }
-}
+open class EventOwnerController(override val repository: EventOwnerRepository) : BaseController<EventOwner, EventOwnerDto>(repository)
--- src/main/kotlin/no/fg/hilflingbackend/repository/EventOwnerRepository.kt
+++ src/main/kotlin/no/fg/hilflingbackend/repository/EventOwnerRepository.kt
@@ -1,25 +1,35 @@
 package no.fg.hilflingbackend.repository
 
 import me.liuwj.ktorm.database.Database
+import me.liuwj.ktorm.dsl.QueryRowSet
 import me.liuwj.ktorm.dsl.eq
 import me.liuwj.ktorm.entity.add
 import me.liuwj.ktorm.entity.find
 import me.liuwj.ktorm.entity.toList
-import no.fg.hilflingbackend.dto.EventOwnerDto
-import no.fg.hilflingbackend.dto.EventOwnerName
+import me.liuwj.ktorm.entity.update
+import no.fg.hilflingbackend.dto.*
+import no.fg.hilflingbackend.model.Categories
 import no.fg.hilflingbackend.model.EventOwner
+import no.fg.hilflingbackend.model.EventOwners
 import no.fg.hilflingbackend.model.event_owners
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
-@Repository
-open class EventOwnerRepository {
-  @Autowired
-  open lateinit var database: Database
 
-  fun findById(id: UUID): EventOwner? {
-    return database.event_owners.find { it.id eq id }
+@Repository
+open class EventOwnerRepository(database: Database) : BaseRepository<EventOwner, EventOwnerDto>(table = EventOwners, database = database) {
+  override fun convertToClass(qrs: QueryRowSet): EventOwnerDto = EventOwnerDto(
+    eventOwnerId = EventOwnerId(qrs[EventOwners.id]!!),
+    name = EventOwnerName.valueOf(qrs[EventOwners.name]!!)
+  )
+
+  override fun create(dto: EventOwnerDto): Int {
+    return database.event_owners.add(dto.toEntity())
+  }
+
+  override fun patch(dto: EventOwnerDto): Int {
+    return database.event_owners.update(dto.toEntity())
   }
 
   @Deprecated("Will be removed when MockDataService is updated. Use findByEventOwnerName instead.")
