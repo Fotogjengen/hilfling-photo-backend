550187a339aaf776050037ae2a36170f4a3e2193
Caroline Sandsbråten <carolinesandsbraten@gmail.com>
2021-02-09 20:50:21 +0100

    update purchase order endpoint

--- src/main/kotlin/no/fg/hilflingbackend/controller/PurchaseOrderController.kt
+++ src/main/kotlin/no/fg/hilflingbackend/controller/PurchaseOrderController.kt
@@ -1,5 +1,6 @@
 package no.fg.hilflingbackend.controller
 
+import no.fg.hilflingbackend.dto.PurchaseOrderDto
 import no.fg.hilflingbackend.model.PurchaseOrder
 import no.fg.hilflingbackend.repository.PurchaseOrderRepository
 import org.springframework.beans.factory.annotation.Autowired
@@ -11,26 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
+
 @RestController
 @RequestMapping("/purchase_orders")
-class PurchaseOrderController {
-  @Autowired
-  lateinit var repository: PurchaseOrderRepository
-
-  @GetMapping("/{id}")
-  fun getById(@PathVariable("id") id: UUID): PurchaseOrder? {
-    return repository.findById(id)
-  }
-
-  @GetMapping
-  fun getAll(): List<PurchaseOrder> {
-    return repository.findAll()
-  }
-
-  @PostMapping
-  fun create(
-    @RequestBody purchaseOrder: PurchaseOrder
-  ): PurchaseOrder {
-    return repository.create(purchaseOrder)
-  }
-}
+open class PurchaseOrderController(override val repository: PurchaseOrderRepository) : BaseController<PurchaseOrder, PurchaseOrderDto>(repository)
--- src/main/kotlin/no/fg/hilflingbackend/repository/PurchaseOrderRepository.kt
+++ src/main/kotlin/no/fg/hilflingbackend/repository/PurchaseOrderRepository.kt
@@ -1,44 +1,44 @@
 package no.fg.hilflingbackend.repository
 
 import me.liuwj.ktorm.database.Database
+import me.liuwj.ktorm.dsl.QueryRowSet
 import me.liuwj.ktorm.dsl.eq
 import me.liuwj.ktorm.entity.add
 import me.liuwj.ktorm.entity.find
 import me.liuwj.ktorm.entity.toList
+import me.liuwj.ktorm.entity.update
+import no.fg.hilflingbackend.dto.PurchaseOrderDto
+import no.fg.hilflingbackend.dto.PurchaseOrderId
+import no.fg.hilflingbackend.dto.toEntity
 import no.fg.hilflingbackend.model.PurchaseOrder
+import no.fg.hilflingbackend.model.PurchaseOrders
 import no.fg.hilflingbackend.model.purchase_orders
+import no.fg.hilflingbackend.value_object.Email
+import no.fg.hilflingbackend.value_object.ZipCode
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
+
 @Repository
-open class PurchaseOrderRepository {
-  @Autowired
-  open lateinit var database: Database
+open class PurchaseOrderRepository(database: Database) : BaseRepository<PurchaseOrder, PurchaseOrderDto>(table = PurchaseOrders, database = database) {
+  override fun convertToClass(qrs: QueryRowSet): PurchaseOrderDto = PurchaseOrderDto(
+    purchaseOrderId = PurchaseOrderId(qrs[PurchaseOrders.id]!!),
+    name = qrs[PurchaseOrders.name]!!,
+    email = Email(qrs[PurchaseOrders.email]!!),
+    address = qrs[PurchaseOrders.address]!!,
+    zipCode = ZipCode(qrs[PurchaseOrders.zipCode]!!),
+    city = qrs[PurchaseOrders.city]!!,
+    sendByPost = qrs[PurchaseOrders.sendByPost]!!,
+    comment = qrs[PurchaseOrders.comment]!!,
+    isCompleted = qrs[PurchaseOrders.isCompleted]!!,
+  )
 
-  fun findById(id: UUID): PurchaseOrder? {
-    return database.purchase_orders.find { it.id eq id }
+  override fun create(dto: PurchaseOrderDto): Int {
+    return database.purchase_orders.add(dto.toEntity())
   }
 
-  fun findAll(): List<PurchaseOrder> {
-    return database.purchase_orders.toList()
+  override fun patch(dto: PurchaseOrderDto): Int {
+    return database.purchase_orders.update(dto.toEntity())
   }
-
-  fun create(
-    purchaseOrder: PurchaseOrder
-  ): PurchaseOrder {
-    val purchaseOrderFromDatabase = PurchaseOrder {
-      this.name = purchaseOrder.name
-      this.email = purchaseOrder.email
-      this.address = purchaseOrder.address
-      this.zipCode = purchaseOrder.zipCode
-      this.city = purchaseOrder.city
-      this.sendByPost = purchaseOrder.sendByPost
-      this.comment = purchaseOrder.comment
-      this.isCompleted = purchaseOrder.isCompleted
-    }
-    database.purchase_orders.add(purchaseOrderFromDatabase)
-    return purchaseOrderFromDatabase
-  }
-  // TODO: DELETE
 }
