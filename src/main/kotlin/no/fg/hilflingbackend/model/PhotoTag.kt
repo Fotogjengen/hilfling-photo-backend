5be8907b0741b52d4b4bdcfc71d3c3b97ba31159
Taheera Ahmed <60318348+taheeraahmed@users.noreply.github.com>
2021-03-20 15:46:45 +0100

    Add PhotoTagController.kt (#95)
    * Add PhotoTagController.kt
    
    * Fix linter

--- src/main/kotlin/no/fg/hilflingbackend/model/PhotoTag.kt
+++ src/main/kotlin/no/fg/hilflingbackend/model/PhotoTag.kt
@@ -4,12 +4,19 @@ import me.liuwj.ktorm.database.Database
 import me.liuwj.ktorm.entity.Entity
 import me.liuwj.ktorm.entity.sequenceOf
 import me.liuwj.ktorm.schema.varchar
+import no.fg.hilflingbackend.dto.PhotoTagDto
+import no.fg.hilflingbackend.dto.PhotoTagId
 
 interface PhotoTag : BaseModel<PhotoTag> {
   companion object : Entity.Factory<PhotoTag>()
 
   var name: String
 }
+
+fun PhotoTag.toDto() = PhotoTagDto(
+  photoTagId = PhotoTagId(),
+  name = this.name
+)
 
 object PhotoTags : BaseTable<PhotoTag>("photo_tag") {
   val name = varchar("name").bindTo { it.name }
