5a8e7d24a6ef837e49b19a5c36847c0408590092
Taheera Ahmed <60318348+taheeraahmed@users.noreply.github.com>
2023-09-21 18:54:52 +0200

    Revert "Fix docker-compose.yml (#291)"
    This reverts commit a475e9719541ab7fc306712f1d74cc199a87bbb0.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 # Stage 1: Build the project
-
 FROM maven:3.6.3-jdk-11-slim AS MAVEN_BUILD
-
+MAINTAINER fg-web@samfundet.no
 # Tomcat creates a working dir at /tmp by default. Might be optional
 VOLUME /tmp
 COPY pom.xml /build/
@@ -20,7 +19,5 @@ RUN apk --no-cache add openjdk11
 WORKDIR /app
 COPY --from=MAVEN_BUILD /build/target/*.jar /app/
 RUN ls /app/
-
 # To reduce startup time: adding /dev/urandom as a source of entropy
 ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app/hilfling-0.0.1-SNAPSHOT.jar"]
-EXPOSE 8000
--- src/main/kotlin/no/fg/hilflingbackend/configurations/ResourceConfiguration.java
+++ src/main/kotlin/no/fg/hilflingbackend/configurations/ResourceConfiguration.java
@@ -3,12 +3,10 @@ package no.fg.hilflingbackend.configurations;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class ResourceConfiguration implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
