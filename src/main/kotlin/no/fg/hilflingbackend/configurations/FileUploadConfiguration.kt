ae0318e37a42d8c8e799aa00bcbefd857bb3f618
Marius Bølset Gisleberg <Marius.marcello@live.no>
2024-10-11 17:20:52 +0200

    added configuration for multipart upload

---
+++ src/main/kotlin/no/fg/hilflingbackend/configurations/FileUploadConfiguration.kt
@@ -0,0 +1,15 @@
+package no.fg.hilflingbackend.configurations
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.multipart.MultipartResolver
+import org.springframework.web.multipart.support.StandardServletMultipartResolver
+
+@Configuration
+class MultipartConfig {
+
+  @Bean
+  fun multipartResolver(): MultipartResolver {
+    return StandardServletMultipartResolver()
+  }
+}
