8f574c345d8de7d0f0d1d5edf210bd2156967731
Marius Bølset Gisleberg <Marius.marcello@live.no>
2025-03-06 19:26:11 +0100

    deleted TODO enpoint not implemented

--- src/main/kotlin/no/fg/hilflingbackend/controller/PhotoController.kt
+++ src/main/kotlin/no/fg/hilflingbackend/controller/PhotoController.kt
@@ -5,21 +5,12 @@ import no.fg.hilflingbackend.dto.Page
 import no.fg.hilflingbackend.dto.PhotoDto
 import no.fg.hilflingbackend.dto.PhotoPatchRequestDto
 import no.fg.hilflingbackend.exceptions.GlobalExceptionHandler
-import no.fg.hilflingbackend.model.AnalogPhoto
 import no.fg.hilflingbackend.service.PhotoService
 import no.fg.hilflingbackend.utils.ResponseOk
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RequestPart
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 import java.security.InvalidParameterException
 import java.time.LocalDate
@@ -115,21 +106,6 @@ class PhotoController(
       HttpHeaders(),
       HttpStatus.CREATED
     )
-  }
-
-  @PostMapping("/analog")
-  fun createAnalogPhoto(
-    @RequestBody analogPhoto: AnalogPhoto
-  ): AnalogPhoto {
-    TODO("Implement")
-  }
-
-  @PatchMapping("/analog")
-  fun uploadAnalogPhoto(
-    @RequestPart("photo") analogPhoto: AnalogPhoto,
-    @RequestPart("file") file: MultipartFile
-  ): ResponseEntity<AnalogPhoto> {
-    TODO("Implement")
   }
 
   @GetMapping("/{id}")
