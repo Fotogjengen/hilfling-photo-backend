7ead65c3fb059db3f1757c450ae3e6dc1f2c94e4
Jenny Lindgaard <jenny.lindgaard@icloud.com>
2025-03-15 10:04:04 +0100

    fix: lint errors

--- src/main/kotlin/no/fg/hilflingbackend/controller/PhotoController.kt
+++ src/main/kotlin/no/fg/hilflingbackend/controller/PhotoController.kt
@@ -29,9 +29,8 @@ import java.util.*
 @RequestMapping("/photos")
 class PhotoController(
   val photoService: PhotoService,
-  val database: Database
+  val database: Database,
 ) : GlobalExceptionHandler() {
-
   // The main photo-upload endpoint used most of the time
   @PostMapping("/upload")
   fun uploadPhotos(
@@ -58,9 +57,9 @@ class PhotoController(
         tagList = tagList,
         categoryName = categoryName,
         isGoodPhotoList = isGoodPhotoList,
-        dateCreated = LocalDate.now()
+        dateCreated = LocalDate.now(),
       ),
-      HttpStatus.CREATED
+      HttpStatus.CREATED,
     )
 
   @PostMapping
@@ -73,7 +72,7 @@ class PhotoController(
     @RequestParam("photoGangBangerIdList") photoGangBangerIdList: List<UUID>,
     @RequestParam("albumIdList") albumIdList: List<UUID>,
     @RequestParam("categoryIdList") categoryIdList: List<UUID>,
-    @RequestParam("fileList") fileList: List<MultipartFile>
+    @RequestParam("fileList") fileList: List<MultipartFile>,
   ): ResponseEntity<List<String>> {
     // Assert all fields are populated
     if (
@@ -82,7 +81,7 @@ class PhotoController(
           placeIdList.size == securityLevelIdList.size &&
           gangIdList.size == photoGangBangerIdList.size &&
           isGoodPictureList.size == fileList.size
-        )
+      )
     ) {
       logger.error("Parameter lists are unequal")
       throw InvalidParameterException("Parameter lists are unequal")
@@ -97,29 +96,30 @@ class PhotoController(
     }.memoize()
      */
 
-    val createdPhotoList = photoService
-      .saveDigitalPhotos(
-        isGoodPictureList,
-        motiveIdList,
-        placeIdList,
-        securityLevelIdList,
-        gangIdList,
-        photoGangBangerIdList,
-        albumIdList,
-        categoryIdList,
-        fileList
-      )
+    val createdPhotoList =
+      photoService
+        .saveDigitalPhotos(
+          isGoodPictureList,
+          motiveIdList,
+          placeIdList,
+          securityLevelIdList,
+          gangIdList,
+          photoGangBangerIdList,
+          albumIdList,
+          categoryIdList,
+          fileList,
+        )
 
     return ResponseEntity<List<String>>(
       createdPhotoList,
       HttpHeaders(),
-      HttpStatus.CREATED
+      HttpStatus.CREATED,
     )
   }
-  
+
   @PostMapping("/analog")
   fun createAnalogPhoto(
-    @RequestBody analogPhoto: AnalogPhoto
+    @RequestBody analogPhoto: AnalogPhoto,
   ): AnalogPhoto {
     TODO("Implement")
   }
@@ -127,26 +127,30 @@ class PhotoController(
   @PatchMapping("/analog")
   fun uploadAnalogPhoto(
     @RequestPart("photo") analogPhoto: AnalogPhoto,
-    @RequestPart("file") file: MultipartFile
+    @RequestPart("file") file: MultipartFile,
   ): ResponseEntity<AnalogPhoto> {
     TODO("Implement")
   }
 
   @GetMapping("/{id}")
-  fun getById(@PathVariable("id") id: UUID): ResponseEntity<PhotoDto> = ResponseOk(
-    photoService
-      .findById(id)
-  )
+  fun getById(
+    @PathVariable("id") id: UUID,
+  ): ResponseEntity<PhotoDto> =
+    ResponseOk(
+      photoService
+        .findById(id),
+    )
 
   @GetMapping("/motive/{id}")
   fun getByMotiveId(
     @PathVariable("id") id: UUID,
     @RequestParam("page", required = false) page: Int?,
-    @RequestParam("pageSize", required = false) pageSize: Int?
-  ): ResponseEntity<Page<PhotoDto>?> = ResponseOk(
-    photoService
-      .getByMotiveId(id, page ?: 0, pageSize ?: 100)
-  )
+    @RequestParam("pageSize", required = false) pageSize: Int?,
+  ): ResponseEntity<Page<PhotoDto>?> =
+    ResponseOk(
+      photoService
+        .getByMotiveId(id, page ?: 0, pageSize ?: 100),
+    )
 
   @GetMapping
   fun getAll(
@@ -188,20 +192,22 @@ class PhotoController(
   @GetMapping("/carousel")
   fun getCarouselPhotos(
     @RequestParam("page", required = false) page: Int?,
-    @RequestParam("pageSize", required = false) pageSize: Int?
-  ): ResponseEntity<Page<PhotoDto>> = ResponseOk(
-    photoService
-      .getCarouselPhotos(page ?: 0, pageSize ?: 6)
-  )
+    @RequestParam("pageSize", required = false) pageSize: Int?,
+  ): ResponseEntity<Page<PhotoDto>> =
+    ResponseOk(
+      photoService
+        .getCarouselPhotos(page ?: 0, pageSize ?: 6),
+    )
 
   @GetMapping("/analog")
   fun getAllAnalogPhotos(
     @RequestParam("page", required = false) page: Int?,
-    @RequestParam("pageSize", required = false) pageSize: Int?
-  ): ResponseEntity<Page<PhotoDto>> = ResponseOk(
-    photoService
-      .getAllAnalogPhotos(page ?: 0, pageSize ?: 100)
-  )
+    @RequestParam("pageSize", required = false) pageSize: Int?,
+  ): ResponseEntity<Page<PhotoDto>> =
+    ResponseOk(
+      photoService
+        .getAllAnalogPhotos(page ?: 0, pageSize ?: 100),
+    )
 
   @GetMapping("/digital")
   fun getAllDigitalPhotos(
@@ -217,8 +223,8 @@ class PhotoController(
     @RequestParam("album", required = false) album: UUID?,
     @RequestParam("sortBy", required = false) sortBy: String?,
     @RequestParam("desc", required = false) desc: Boolean?,
-  ): ResponseEntity<Page<PhotoDto>> {
-    return ResponseOk(
+  ): ResponseEntity<Page<PhotoDto>> =
+    ResponseOk(
       photoService
         .getAllDigitalPhotos(
           page ?: 0,
@@ -232,15 +238,12 @@ class PhotoController(
           isGoodPic ?: false,
           album ?: UUID(0L, 0L),
           sortBy ?: "",
-          desc ?: true
-        )
+          desc ?: true,
+        ),
     )
-  }
 
   @PatchMapping
   fun patch(
-    @RequestBody dto: PhotoPatchRequestDto
-  ): PhotoDto {
-    return photoService.patch(dto)
-  }
+    @RequestBody dto: PhotoPatchRequestDto,
+  ): PhotoDto = photoService.patch(dto)
 }
