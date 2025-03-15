1e2202a00086ec099b955a45155bdca6a3a41dbd
Jenny Lindgaard <jenny.lindgaard@icloud.com>
2025-03-15 10:11:54 +0100

    fix: lint errors

--- src/main/kotlin/no/fg/hilflingbackend/service/IPhotoService.kt
+++ src/main/kotlin/no/fg/hilflingbackend/service/IPhotoService.kt
@@ -9,6 +9,7 @@ import java.util.UUID
 
 interface IBaseService<T> {
   fun getById(id: UUID): PhotoDto?
+
   fun getAll(
     page: Int = 0,
     pageSize: Int = 100,
@@ -23,9 +24,10 @@ interface IBaseService<T> {
     sortBy: String,
     desc: Boolean = true,
     securityLevel: String,
-    isAnalog: Boolean = false
+    isAnalog: Boolean = false,
   ): Page<T>
 }
+
 interface IPhotoService : IBaseService<PhotoDto> {
   fun saveDigitalPhotos(
     isGoodPictureList: List<Boolean>,
@@ -36,8 +38,9 @@ interface IPhotoService : IBaseService<PhotoDto> {
     photoGangBangerIdList: List<UUID>,
     albumIdList: List<UUID>,
     categoryIdList: List<UUID>,
-    fileList: List<MultipartFile>
+    fileList: List<MultipartFile>,
   ): List<String>
+
   fun saveAnalogPhotos(
     isGoodPictureList: List<Boolean>,
     motiveIdList: List<UUID>,
@@ -45,8 +48,9 @@ interface IPhotoService : IBaseService<PhotoDto> {
     securityLevelIdList: List<UUID>,
     gangIdList: List<UUID>,
     photoGangBangerIdList: List<UUID>,
-    fileList: List<MultipartFile>
+    fileList: List<MultipartFile>,
   ): List<String>
+
   fun createNewMotiveAndSaveDigitalPhotos(
     motiveString: String,
     placeString: String,
@@ -60,7 +64,12 @@ interface IPhotoService : IBaseService<PhotoDto> {
     dateCreated: LocalDate,
     tagList: List<String>,
   ): List<String>
-  fun getCarouselPhotos(page: Int = 0, pageSize: Int = 100): Page<PhotoDto>
+
+  fun getCarouselPhotos(
+    page: Int = 0,
+    pageSize: Int = 100,
+  ): Page<PhotoDto>
+
   fun getAllAnalogPhotos(page: Int = 0, pageSize: Int = 100): Page<PhotoDto> // TODO: Need different DTO for analog
 
   fun getAllDigitalPhotos(
@@ -75,7 +84,7 @@ interface IPhotoService : IBaseService<PhotoDto> {
     isGoodPic: Boolean = false,
     album: UUID,
     sortBy: String,
-    desc: Boolean = true
+    desc: Boolean = true,
   ): Page<PhotoDto>
 
   fun patch(dto: PhotoPatchRequestDto): PhotoDto
