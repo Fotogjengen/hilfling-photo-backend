2a906905e2cb3d2fdbb2dc005a5d67e7db590a12
Philip Torvund Bennett <philitb@stud.ntnu.no>
2025-03-21 19:30:32 +0100

    Fixed wildcard imports

--- src/main/kotlin/no/fg/hilflingbackend/repository/EventCardRepository.kt
+++ src/main/kotlin/no/fg/hilflingbackend/repository/EventCardRepository.kt
@@ -1,10 +1,19 @@
 package no.fg.hilflingbackend.repository
 
 import me.liuwj.ktorm.database.Database
-import me.liuwj.ktorm.dsl.*
-import me.liuwj.ktorm.entity.map
+import me.liuwj.ktorm.dsl.and
+import me.liuwj.ktorm.dsl.eq
+import me.liuwj.ktorm.dsl.from
+import me.liuwj.ktorm.dsl.innerJoin
+import me.liuwj.ktorm.dsl.limit
+import me.liuwj.ktorm.dsl.map
+import me.liuwj.ktorm.dsl.select
+import me.liuwj.ktorm.dsl.where
 import no.fg.hilflingbackend.dto.EventCardDto
-import no.fg.hilflingbackend.model.*
+import no.fg.hilflingbackend.model.EventOwner
+import no.fg.hilflingbackend.model.EventOwners
+import no.fg.hilflingbackend.model.Motives
+import no.fg.hilflingbackend.model.Photos
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Repository
 
@@ -34,7 +43,7 @@ open class EventCardRepository {
             database
               .from(Photos)
               .select(Photos.smallUrl)
-              .where { (Photos.motiveId eq id) and (Photos.isGoodPicture eq true) }
+              .where { (Photos.motiveId eq id).and(Photos.isGoodPicture eq true) }
               .limit(0, 1) // Only fetch one record
               .map { it[Photos.smallUrl] }
               .firstOrNull()
