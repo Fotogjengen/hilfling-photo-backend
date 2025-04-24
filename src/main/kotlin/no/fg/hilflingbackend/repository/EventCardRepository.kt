8b35e52141939ded7186fb244edadc152825d27f
Philip Torvund Bennett <philitb@stud.ntnu.no>
2025-04-24 19:27:09 +0200

    Added endpoint for fetching eventcards without eventowners

--- src/main/kotlin/no/fg/hilflingbackend/repository/EventCardRepository.kt
+++ src/main/kotlin/no/fg/hilflingbackend/repository/EventCardRepository.kt
@@ -5,11 +5,13 @@ import me.liuwj.ktorm.dsl.and
 import me.liuwj.ktorm.dsl.eq
 import me.liuwj.ktorm.dsl.from
 import me.liuwj.ktorm.dsl.innerJoin
+import me.liuwj.ktorm.dsl.like
 import me.liuwj.ktorm.dsl.limit
 import me.liuwj.ktorm.dsl.map
 import me.liuwj.ktorm.dsl.select
 import me.liuwj.ktorm.dsl.where
 import no.fg.hilflingbackend.dto.EventCardDto
+import no.fg.hilflingbackend.dto.Page
 import no.fg.hilflingbackend.model.EventOwner
 import no.fg.hilflingbackend.model.EventOwners
 import no.fg.hilflingbackend.model.Motives
@@ -58,4 +60,140 @@ open class EventCardRepository {
           frontPageSmallPhotoUrl = selectedPhotoUrl,
         )
       }
+
+  fun getAllPaginatedEventCards(
+    page: Int = 0,
+    pageSize: Int = 10,
+  ): Page<EventCardDto> {
+    val eventCards =
+      database
+        .from(Motives)
+        .innerJoin(EventOwners, on = Motives.eventOwnerId eq EventOwners.id)
+        .select(
+          Motives.id,
+          Motives.title,
+          Motives.dateCreated,
+          EventOwners.name,
+        ).limit(page * pageSize, pageSize)
+        .map { row ->
+          val motiveId = row[Motives.id]
+          val selectedPhotoUrl =
+            motiveId?.let { id ->
+              database
+                .from(Photos)
+                .select(Photos.smallUrl)
+                .where { (Photos.motiveId eq id).and(Photos.isGoodPicture eq true) }
+                .limit(0, 1)
+                .map { it[Photos.smallUrl] }
+                .firstOrNull()
+            }
+
+          EventCardDto(
+            motiveId = motiveId,
+            motiveTitle = row[Motives.title],
+            date_created = row[Motives.dateCreated],
+            eventOwnerName = row[EventOwners.name],
+            frontPageSmallPhotoUrl = selectedPhotoUrl,
+          )
+        }
+    return Page(
+      page = page,
+      pageSize = pageSize,
+      totalRecords = eventCards.size,
+      currentList = eventCards,
+    )
+  }
+
+  // Get paginated event cards
+  fun getPaginatedEventCards(
+    eventOwner: EventOwner,
+    page: Int = 0,
+    pageSize: Int = 10,
+  ): Page<EventCardDto> {
+    val eventCards =
+      database
+        .from(Motives)
+        .innerJoin(EventOwners, on = Motives.eventOwnerId eq EventOwners.id)
+        .select(
+          Motives.id,
+          Motives.title,
+          Motives.dateCreated,
+          EventOwners.name,
+        ).where { EventOwners.name eq eventOwner.name }
+        .limit(page * pageSize, pageSize)
+        .map { row ->
+          val motiveId = row[Motives.id]
+          val selectedPhotoUrl =
+            motiveId?.let { id ->
+              database
+                .from(Photos)
+                .select(Photos.smallUrl)
+                .where { (Photos.motiveId eq id).and(Photos.isGoodPicture eq true) }
+                .limit(0, 1)
+                .map { it[Photos.smallUrl] }
+                .firstOrNull()
+            }
+
+          EventCardDto(
+            motiveId = motiveId,
+            motiveTitle = row[Motives.title],
+            date_created = row[Motives.dateCreated],
+            eventOwnerName = row[EventOwners.name],
+            frontPageSmallPhotoUrl = selectedPhotoUrl,
+          )
+        }
+    return Page(
+      page = page,
+      pageSize = pageSize,
+      totalRecords = eventCards.size,
+      currentList = eventCards,
+    )
+  }
+
+  // Search event cards by term
+  fun searchEventCards(
+    eventOwner: EventOwner,
+    searchTerm: String,
+    page: Int = 0,
+    pageSize: Int = 10,
+  ): Page<EventCardDto> {
+    val eventCards =
+      database
+        .from(Motives)
+        .innerJoin(EventOwners, on = Motives.eventOwnerId eq EventOwners.id)
+        .select(
+          Motives.id,
+          Motives.title,
+          Motives.dateCreated,
+          EventOwners.name,
+        ).where { (Motives.title like "%$searchTerm%") and (EventOwners.name eq eventOwner.name) }
+        .limit(page * pageSize, pageSize)
+        .map { row ->
+          val motiveId = row[Motives.id]
+          val selectedPhotoUrl =
+            motiveId?.let { id ->
+              database
+                .from(Photos)
+                .select(Photos.smallUrl)
+                .where { (Photos.motiveId eq id).and(Photos.isGoodPicture eq true) }
+                .limit(0, 1)
+                .map { it[Photos.smallUrl] }
+                .firstOrNull()
+            }
+
+          EventCardDto(
+            motiveId = motiveId,
+            motiveTitle = row[Motives.title],
+            date_created = row[Motives.dateCreated],
+            eventOwnerName = row[EventOwners.name],
+            frontPageSmallPhotoUrl = selectedPhotoUrl,
+          )
+        }
+    return Page(
+      page = page,
+      pageSize = pageSize,
+      totalRecords = eventCards.size,
+      currentList = eventCards,
+    )
+  }
 }
