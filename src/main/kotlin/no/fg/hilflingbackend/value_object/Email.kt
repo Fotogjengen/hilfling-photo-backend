66013ed0c26112ff74c643d3758dddc6b4a2c855
rubensen1 <150120676+rubensen1@users.noreply.github.com>
2026-02-21 16:27:50 +0100

    debug text and changed backend regex (#374)
    * debug text and changed backend regex
    
    * NO debug line in finished commit

--- src/main/kotlin/no/fg/hilflingbackend/value_object/Email.kt
+++ src/main/kotlin/no/fg/hilflingbackend/value_object/Email.kt
@@ -10,18 +10,14 @@ data class Email private constructor(
       if (value != null && isEmailValid(value)) {
         return Email(value = value)
       }
+
       throw IllegalArgumentException("Not an email")
     }
 
     fun isEmailValid(email: String): Boolean =
       Pattern
         .compile(
-          "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]|[\\w-]{2,}))@" +
-            "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?" +
-            "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\." +
-            "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?" +
-            "[0-9]{1,2}|25[0-5]|2[0-4][0-9]))|" +
-            "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$",
+          "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-zA-Z]{2,}$",
         ).matcher(email)
         .matches()
   }
